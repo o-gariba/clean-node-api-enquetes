0c5c563ad025884ca5fa77219f9644855610a35a
Pedro Garibaldi Santos <pedro.garibaldi.santos@alumni.usp.br>
2025-04-27 16:01:45 -0300

    test: add no email provided test

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,7 +1,7 @@
 import {SignUpController} from "./signup";
 
 describe('SignUp Controller', () => {
-    it('Must return 400 if no email is provided', () => {
+    it('Must return 400 if no name is provided', () => {
         const sut = new SignUpController()
         const httpRequest = {
             body: {
@@ -13,5 +13,19 @@ describe('SignUp Controller', () => {
         const httpResponse = sut.handle(httpRequest)
         expect(httpResponse.statusCode).toBe(400)
         expect(httpResponse.body).toEqual(new Error('Missing param: name'))
+    })
+
+    it('Must return 400 if no email is provided', () => {
+        const sut = new SignUpController()
+        const httpRequest = {
+            body: {
+                name: 'name',
+                password: '123',
+                passwordConfirmation: '123'
+            }
+        }
+        const httpResponse = sut.handle(httpRequest)
+        expect(httpResponse.statusCode).toBe(400)
+        expect(httpResponse.body).toEqual(new Error('Missing param: email'))
     })
 })

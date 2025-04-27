e3ac9754d156b96802bc2643ac1636415838f48d
Pedro Garibaldi Santos <pedro.garibaldi.santos@alumni.usp.br>
2025-04-27 16:36:56 -0300

    test: ensure signup controller returns 400 if no password is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -29,4 +29,18 @@ describe('SignUp Controller', () => {
         expect(httpResponse.statusCode).toBe(400)
         expect(httpResponse.body).toEqual(new MissingParamError('email'))
     })
+
+    it('Must return 400 if no password is provided', () => {
+        const sut = new SignUpController()
+        const httpRequest = {
+            body: {
+                name: 'name',
+                email:'email@email.com',
+                passwordConfirmation: '123'
+            }
+        }
+        const httpResponse = sut.handle(httpRequest)
+        expect(httpResponse.statusCode).toBe(400)
+        expect(httpResponse.body).toEqual(new MissingParamError('password'))
+    })
 })

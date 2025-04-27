d534239eee4c0b885a2b955f26d66a5a8a52479e
Pedro Garibaldi Santos <pedro.garibaldi.santos@alumni.usp.br>
2025-04-27 15:54:56 -0300

    test: add error message test

---
+++ src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,17 @@
+import {SignUpController} from "./signup";
+
+describe('SignUp Controller', () => {
+    it('Must return 400 if no email is provided', () => {
+        const sut = new SignUpController()
+        const httpRequest = {
+            body: {
+                email: 'email@email.com',
+                password: '123',
+                passwordConfirmation: '123'
+            }
+        }
+        const httpResponse = sut.handle(httpRequest)
+        expect(httpResponse.statusCode).toBe(400)
+        expect(httpResponse.body).toEqual(new Error('Missing param: name'))
+    })
+})
